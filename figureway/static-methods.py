337ea80e9e5f301f841dd6c115cee554b82a185d
Lev Goldort <lev.goldort@gmail.com>
2021-12-27 02:08:11 +0200

    reworked static methods with bounding box

--- figureway/static-methods.py
+++ figureway/static-methods.py
@@ -9,7 +9,8 @@ import sys
 import requests
 import configparser
 import csv
-
+import overpy
+import math
 
 NAME_EN_TAG = "name:en"
 STREET_HIGHWAY_TAGS = frozenset([  # OpenStreetMap tags related to roads.
@@ -22,72 +23,129 @@ STREET_HIGHWAY_TAGS = frozenset([  # OpenStreetMap tags related to roads.
     "unclassified"
 ])
 CITY_RAD = 10000  # Minimum distance to the closest city
+CITY_RAD_KM = 10
+BASIC_OVERPASS_URL = 'https://overpass.kumi.systems/api/interpreter'
 
-#
-# # degrees to radians
-# # https://stackoverflow.com/questions/238260/how-to-calculate-the-bounding-box-for-a-given-lat-lng-location
-# def deg2rad(degrees):
-#     return math.pi * degrees / 180.0
-#
-#
-# # radians to degrees
-# def rad2deg(radians):
-#     return 180.0 * radians/math.pi
-#
-#
-# # Semi-axes of WGS-84 geoidal reference
-# WGS84_a = 6378137.0  # Major semiaxis [m]
-# WGS84_b = 6356752.3  # Minor semiaxis [m]
-#
-#
-# # Earth radius at a given latitude, according to the WGS-84 ellipsoid [m]
-# def WGS84EarthRadius(lat):
-#     # http://en.wikipedia.org/wiki/Earth_radius
-#     An = WGS84_a * WGS84_a * math.cos(lat)
-#     Bn = WGS84_b * WGS84_b * math.sin(lat)
-#     Ad = WGS84_a * math.cos(lat)
-#     Bd = WGS84_b * math.sin(lat)
-#     return math.sqrt((An * An + Bn * Bn)/(Ad * Ad + Bd * Bd))
-#
-#
-# # Bounding box surrounding the point at given coordinates,
-# # assuming local approximation of Earth surface as a sphere
-# # of radius given by WGS84
-# def boundingBox(latitudeInDegrees, longitudeInDegrees, halfSideInKm):
-#     lat = deg2rad(latitudeInDegrees)
-#     lon = deg2rad(longitudeInDegrees)
-#     halfSide = 1000*halfSideInKm
-#
-#     # Radius of Earth at given latitude
-#     radius = WGS84EarthRadius(lat)
-#     # Radius of the parallel at given latitude
-#     pradius = radius*math.cos(lat)
-#
-#     latMin = lat - halfSide/radius
-#     latMax = lat + halfSide/radius
-#     lonMin = lon - halfSide/pradius
-#     lonMax = lon + halfSide/pradius
-#
-#     return (rad2deg(latMin), rad2deg(lonMin), rad2deg(latMax), rad2deg(lonMax))]
-#
-#
-# def load_ways(min_lat, min_lon, max_lat, max_lon):
-#     api = overpy.Overpass(url='https://overpass.kumi.systems/api/interpreter')
-#
-#     # fetch all ways and nodes
-#     result = api.query("""
-#         way({},{},{},{}) ["highway"];
-#         (._;>;);
-#         out body;
-#         """.format(min_lat, min_lon, max_lat, max_lon))
-#
-#     for way in result.ways:
-#         print("Name: %s" % way.tags.get("name", "n/a"))
-#         print("  Highway: %s" % way.tags.get("highway", "n/a"))
-#         print("  Nodes:")
-#         for node in way.nodes:
-#             print(node.id)
-#             print("    Lat: %f, Lon: %f" % (node.lat, node.lon))
+
+class CityDownloader():
+
+    def __init__(self, city_center_lat, city_center_lon, city_radius, url=BASIC_OVERPASS_URL):
+        self.min_lat, self.min_lon, self.max_lat, self.max_lon = boundingBox(city_center_lat, city_center_lon,
+                                                                             city_radius)
+        self.overpass_url = url
+
+        self.all_ways = self.load_ways(self.min_lat, self.min_lon, self.max_lat, self.max_lon)
+        self.all_way_dots = {}
+        self.way_counter = {}
+        self.node_ways = {}  # ways, connected to the node
+        self.way_nodes = {}  # nodes, contained in the way
+        self.crossroads = {}
+        self.run_ways()
+
+    def load_ways(self, min_lat, min_lon, max_lat, max_lon):
+        api = overpy.Overpass(url=self.overpass_url)
+
+        # fetch all ways and nodes
+        result = api.query("""
+            way({},{},{},{}) ["highway"];
+            (._;>;);
+            out body;
+            """.format(min_lat, min_lon, max_lat, max_lon))
+
+        # for way in result.ways:
+        #     print("Name: %s" % way.tags.get("name:en", "n/a"))
+        #     # print("  Highway: %s" % way.tags.get("highway", "n/a"))
+        #     # print("  Nodes:")
+        #     # for node in way.nodes:
+        #     #     print(node.id)
+        #     #     print("    Lat: %f, Lon: %f" % (node.lat, node.lon))
+        return result
+
+    def run_ways(self):
+        for w in self.all_ways.ways:
+            street_tag = get_street_tag(w.tags)
+            if not street_tag:
+                continue
+
+            self.way_nodes[w.id] = set()
+            for node in w.nodes:
+                self.way_nodes[w.id].add(node.id)
+                if node.id not in self.all_way_dots.keys():
+                    # If node is not saved yet, then add it to dict of all dots
+                    self.all_way_dots[node.id] = {
+                        "lat": float(node.lat),
+                        "lon": float(node.lon),
+                        "x": float(node.lat),
+                        "y": float(node.lon),
+                    }
+                    self.way_counter[node.id] = 0  # Number of different ways this node participate in
+                self.way_counter[node.id] += 1
+                if node.id not in self.node_ways.keys():
+                    self.node_ways[node.id] = {w.id}
+                else:
+                    self.node_ways[node.id].add(w.id)
+
+    def get_neighbors(self, node_ref):
+        result = set()
+        for way in self.node_ways[node_ref]:
+            result = set.union(result, self.way_nodes[way])
+        result.remove(node_ref)
+        return result
+
+    def pick_crossroads(self):
+        crossroads_refs = {key for (key, value) in self.way_counter.items() if value > 1}  # pick all crossroad ids
+        crossroads = {key: self.all_way_dots[key] for key in crossroads_refs}
+        for node_ref in crossroads:  # Saving in neighbors only nodes that are crossroads
+            node_neighbors = self.get_neighbors(node_ref)
+            crossroads[node_ref]['neighbors'] = node_neighbors & crossroads_refs
+        return crossroads
+
+
+# degrees to radians
+# https://stackoverflow.com/questions/238260/how-to-calculate-the-bounding-box-for-a-given-lat-lng-location
+def deg2rad(degrees):
+    return math.pi * degrees / 180.0
+
+
+# radians to degrees
+def rad2deg(radians):
+    return 180.0 * radians / math.pi
+
+
+# Semi-axes of WGS-84 geoidal reference
+WGS84_a = 6378137.0  # Major semiaxis [m]
+WGS84_b = 6356752.3  # Minor semiaxis [m]
+
+
+# Earth radius at a given latitude, according to the WGS-84 ellipsoid [m]
+def WGS84EarthRadius(lat):
+    # http://en.wikipedia.org/wiki/Earth_radius
+    An = WGS84_a * WGS84_a * math.cos(lat)
+    Bn = WGS84_b * WGS84_b * math.sin(lat)
+    Ad = WGS84_a * math.cos(lat)
+    Bd = WGS84_b * math.sin(lat)
+    return math.sqrt((An * An + Bn * Bn) / (Ad * Ad + Bd * Bd))
+
+
+# Bounding box surrounding the point at given coordinates,
+# assuming local approximation of Earth surface as a sphere
+# of radius given by WGS84
+def boundingBox(latitudeInDegrees, longitudeInDegrees, halfSideInKm):
+    lat = deg2rad(latitudeInDegrees)
+    lon = deg2rad(longitudeInDegrees)
+    halfSide = 1000 * halfSideInKm
+
+    # Radius of Earth at given latitude
+    radius = WGS84EarthRadius(lat)
+    # Radius of the parallel at given latitude
+    pradius = radius * math.cos(lat)
+
+    latMin = lat - halfSide / radius
+    latMax = lat + halfSide / radius
+    lonMin = lon - halfSide / pradius
+    lonMax = lon + halfSide / pradius
+
+    return rad2deg(latMin), rad2deg(lonMin), rad2deg(latMax), rad2deg(lonMax)
 
 
 def load_city_list(file_path):
@@ -99,12 +157,18 @@ def load_city_list(file_path):
 
 def download_osm(url, dest):
     r = requests.get(url, allow_redirects=True)
+    print('what we got here', r)
     with open(dest, 'wb') as file:
         file.write(r.content)
 
 
-def drop_crossroads(osm_file_path, cities_json, destination_folder_path, destination_type='local',
-                    destination_bucket=None, s3_api_key=None, s3_secret_key=None, country_filter=[], city_filter=[]):
+def filter_cities(cities_dict, countrycode):
+    return [element for element in cities_dict if element['country'] == countrycode]
+
+
+def drop_crossroads_from_osm(osm_file_path, cities_json, destination_folder_path, destination_type='local',
+                             destination_bucket=None, s3_api_key=None, s3_secret_key=None, country_filter=[],
+                             city_filter=[]):
     # Getting the crossroads from osm_file_path, matches them with cities_json and drops to destination_folder_path
     w = GenerateGraphHandler()
     start = time.time()
@@ -134,6 +198,21 @@ def drop_crossroads(osm_file_path, cities_json, destination_folder_path, destina
             bucket = s3.create_bucket(Bucket=destination_bucket)
             result = s3.Object(destination_bucket, destination_path).put(Body=crossroads_bytes)
 
+
+def drop_crossroads(crossroads, destination_path, destination_type='local',
+                    destination_bucket=None, s3_api_key=None, s3_secret_key=None):
+    # Drops crossroads dict to destination path tp AWS S3 or to local folder
+    if destination_type == 'local':
+        with open(destination_path, 'wb') as handle:
+            pickle.dump(crossroads, handle)
+    else:
+        session = boto3.Session(aws_access_key_id=s3_api_key, aws_secret_access_key=s3_secret_key)
+        s3 = session.resource('s3')
+        crossroads_bytes = pickle.dumps(crossroads)
+        bucket = s3.create_bucket(Bucket=destination_bucket)
+        result = s3.Object(destination_bucket, destination_path).put(Body=crossroads_bytes)
+
+
 def load_crossroads(file_path):
     with open(file_path, 'rb') as handle:
         return pickle.load(handle)
@@ -152,7 +231,6 @@ def find_closest_city(lat, lon, city_list):
         return None
     else:
         return closest_city
-
 
 
 def get_street_tag(tags):
@@ -176,14 +254,16 @@ class GenerateGraphHandler(osmium.SimpleHandler):
         super().__init__()
         self.all_way_dots = {}
         self.way_counter = {}
+        self.node_ways = {}  # ways, connected to the node
+        self.way_nodes = {}  # nodes, contained in the way
 
     def way(self, w):
         street_tag = get_street_tag(w.tags)
         if not street_tag:
             return
-        print(dir(w.nodes))
-        print(w.nodes.__repr__)
+        self.way_nodes[w.id] = set()
         for node in w.nodes:
+            self.way_nodes[w.id].add(node.ref)
             if node.ref not in self.all_way_dots.keys():
                 # If node is not saved yet, then add it to dict of all dots
                 self.all_way_dots[node.ref] = {
@@ -191,33 +271,39 @@ class GenerateGraphHandler(osmium.SimpleHandler):
                     "lon": node.lon,
                     "x": node.x,
                     "y": node.y,
-                    "neighbors": set()
                 }
                 self.way_counter[node.ref] = 0  # Number of different ways this node participate in
-            for connected_node in w.nodes:
-                # Add all nodes in the way as neighbors for this dot
-                self.all_way_dots[node.ref]["neighbors"].add(connected_node.ref)
-            self.all_way_dots[node.ref]["neighbors"].remove(node.ref)
             self.way_counter[node.ref] += 1
+            if node.ref not in self.node_ways.keys():
+                self.node_ways[node.ref] = {w.id}
+            else:
+                self.node_ways[node.ref].add(w.id)
+
+    def get_neighbors(self, node_ref):
+        result = set()
+        for way in self.node_ways[node_ref]:
+            result = set.union(result, self.way_nodes[way])
+        result.remove(node_ref)
+        return result
 
     def pick_crossroads(self, lat, lon, radius):
         crossroads_refs = {key for (key, value) in self.way_counter.items() if value > 1}  # pick all crossroad ids
         crossroads = {key: self.all_way_dots[key] for key in crossroads_refs if  # Match crossroads ids to node data
                       distance(self.all_way_dots[key]['lat'], self.all_way_dots[key]['lon'], lat, lon) < radius}
         for node_ref in crossroads:  # Saving in neighbors only nodes that are crossroads
-            crossroads[node_ref]['neighbors'] = crossroads[node_ref]['neighbors'] & crossroads_refs
+            node_neighbors = self.get_neighbors(node_ref)
+            crossroads[node_ref]['neighbors'] = node_neighbors & crossroads_refs
         return crossroads
 
 
 def download_and_drop_country(cfg, countrycode):
-
     cfg.read(os.path.join(os.path.dirname(__file__), 'config.cfg'))
     city_list_path = cfg.get('INPUT_PATH', 'city_list', fallback='Wrong Config file')
     country_download_links = cfg.get('INPUT_PATH', 'country_list', fallback='Wrong Config file')
 
     destination_type = cfg.get('OUTPUT_PATH', 'destination_type', fallback='Wrong Config file')
     destination_path = cfg.get('OUTPUT_PATH', 'destination_path', fallback='Wrong Config file') + countrycode + '/'
-    #destination_path = os.path.join(os.path.dirname(__file__), destination_folder_path)
+    # destination_path = os.path.join(os.path.dirname(__file__), destination_folder_path)
 
     amazon_access_key_id = cfg.get('DEFAULT', 'aws_access_key_id')
     amazon_secret_key = cfg.get('DEFAULT', 'aws_secret_access_key')
@@ -231,26 +317,94 @@ def download_and_drop_country(cfg, countrycode):
 
     city_list = load_city_list(os.path.join(os.path.dirname(__file__), city_list_path))
 
-    drop_crossroads(dest, city_list,
-                    destination_folder_path=destination_path,
-                    country_filter=[countrycode],
-                    city_filter='',
-                    destination_type=destination_type,
-                    s3_api_key=amazon_access_key_id,
-                    s3_secret_key=amazon_secret_key,
-                    destination_bucket=amazon_bucket_name)
+    drop_crossroads_from_osm(dest, city_list,
+                             destination_folder_path=destination_path,
+                             country_filter=[countrycode],
+                             city_filter='',
+                             destination_type=destination_type,
+                             s3_api_key=amazon_access_key_id,
+                             s3_secret_key=amazon_secret_key,
+                             destination_bucket=amazon_bucket_name)
 
     os.remove(dest)
 
 
-def main():
-    start = time.time()
+def old_main():
+    # start = time.time()
+    # cfg = configparser.ConfigParser()
+    # download_and_drop_country(cfg, 'NL')
+    # print('all executed in:', time.time() - start)
+
     cfg = configparser.ConfigParser()
-    download_and_drop_country(cfg, 'MD')
-    print('all executed in:', time.time() - start)
+    cfg.read(os.path.join(os.path.dirname(__file__), 'config.cfg'))
+    city_list_path = cfg.get('INPUT_PATH', 'city_list', fallback='Wrong Config file')
+    country_download_links = cfg.get('INPUT_PATH', 'country_list', fallback='Wrong Config file')
+
+    destination_type = cfg.get('OUTPUT_PATH', 'destination_type', fallback='Wrong Config file')
+    destination_path = cfg.get('OUTPUT_PATH', 'destination_path', fallback='Wrong Config file') + 'RU' + '/'
+    # destination_path = os.path.join(os.path.dirname(__file__), destination_folder_path)
+
+    amazon_access_key_id = cfg.get('DEFAULT', 'aws_access_key_id')
+    amazon_secret_key = cfg.get('DEFAULT', 'aws_secret_access_key')
+    amazon_bucket_name = cfg.get('DEFAULT', 'aws_s3_bucket')
+
+    country_links_list = list(csv.reader(open(os.path.join(os.path.dirname(__file__), country_download_links))))
+    # country = [element for element in country_links_list if element[0] == countrycode][0]
+
+    dest = '/Users/levgoldort/Downloads/russia-latest.osm.pbf'
+
+    city_list = load_city_list(os.path.join(os.path.dirname(__file__), city_list_path))
+
+    drop_crossroads_from_osm(dest, city_list,
+                             destination_folder_path=destination_path,
+                             country_filter=['RU'],
+                             city_filter='',
+                             destination_type=destination_type,
+                             s3_api_key=amazon_access_key_id,
+                             s3_secret_key=amazon_secret_key,
+                             destination_bucket=amazon_bucket_name)
 
 
+def main(countrycode):
+    cfg = configparser.ConfigParser()
+    cfg.read(os.path.join(os.path.dirname(__file__), 'config.cfg'))
+    city_list_path = cfg.get('INPUT_PATH', 'city_list', fallback='Wrong Config file')
+    country_download_links = cfg.get('INPUT_PATH', 'country_list', fallback='Wrong Config file')
+
+    destination_type = cfg.get('OUTPUT_PATH', 'destination_type', fallback='Wrong Config file')
+    # destination_path = os.path.join(os.path.dirname(__file__), destination_folder_path)
+
+    amazon_access_key_id = cfg.get('DEFAULT', 'aws_access_key_id')
+    amazon_secret_key = cfg.get('DEFAULT', 'aws_secret_access_key')
+    amazon_bucket_name = cfg.get('DEFAULT', 'aws_s3_bucket')
+
+    city_list = load_city_list(os.path.join(os.path.dirname(__file__), city_list_path))
+    filtered_cities = filter_cities(city_list, countrycode)
+    destination_path = cfg.get('OUTPUT_PATH', 'destination_path', fallback='Wrong Config file') + countrycode + '/'
+    failure_list = []
+    for city in filtered_cities:
+        print(city)
+        start_time = time.time()
+        try:
+            city_finder = CityDownloader(float(city['lat']), float(city['lng']), 10)
+            crossroads = city_finder.pick_crossroads()
+            upload_path = destination_path + city['lat'] + city['lng'] + '.pickle'
+            drop_crossroads(crossroads, upload_path, destination_type='bucket', destination_bucket=amazon_bucket_name,
+                            s3_api_key=amazon_access_key_id, s3_secret_key=amazon_secret_key)
+            print('crossroads for {} exectued in {}'.format(city['name'], time.time() - start_time))
+
+        except:
+            failure_list.add(city)
+            print('The city {} was not read succesfully :('.format(city['name']))
+            time.sleep(300)
+
+        if failure_list:
+            failure_string = str(failure_list)
+            with open('failure.txt', 'w') as handle:
+                handle.write(failure_string)
 
 
 if __name__ == '__main__':
-    main()
+    start_time = time.time()
+    main('RU')
+    print('whole script executed in', time.time() - start_time)
